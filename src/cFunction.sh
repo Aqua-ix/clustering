192784597e0f3a15a43ca2161cbb2d28324587dd
Aqua-ix <souichi1997@gmail.com>
2018-12-12 13:02:19 +0900

    update

--- src/cFunction.sh
+++ src/cFunction.sh
@@ -3,8 +3,7 @@
 cd ~/clustering/src
 
 gnuplot <<EOF
-splot "sFCMA-Em2.000000-2d-Gaussian-2clusters.result_classificationFunction" using 1:2:3 with lines, "sFCMA-Em2.000000-2d-Gaussian-2clusters.result_classificationFunction" using 1:2:4 with lines, "< awk '{if($3>$4){print $1, $2, $3;}}' sFCMA-Em2.000000-2d-Gaussian-2clusters.result_membership", "< awk '{if($4>$3){print $1, $2, $4;}}' sFCMA-Em2.000000-2d-Gaussian-2clusters.result_membership"
-set hidden3d
+splot "qFCMA-Em2.000000-Lambda10000.000000-2d-Gaussian-2clusters.result_classificationFunction" using 1:2:3 with lines, "qFCMA-Em2.000000-Lambda10000.000000-2d-Gaussian-2clusters.result_classificationFunction" using 1:2:4 with lines, "< awk '{if($3>$4){print $1, $2, $3;}}' qFCMA-Em2.000000-Lambda10000.000000-2d-Gaussian-2clusters.result_membership", "< awk '{if($4>$3){print $1, $2, $4;}}' qFCMA-Em2.000000-Lambda10000.000000-2d-Gaussian-2clusters.result_membership"
 replot
 set ticslevel 0
 set key outside center bottom
--- src/qfcma_main_2d-Gaussian-2clusters.cxx
+++ src/qfcma_main_2d-Gaussian-2clusters.cxx
@@ -11,7 +11,7 @@ const int centers_number=2;
 
 int main(void){
   double Em=2.0;
-  double Lambda=10.0;
+  double Lambda=10000.0;
   
   std::string filenameData("2d-Gaussian-2clusters.dat");
 #ifdef CHECK_ANSWER
