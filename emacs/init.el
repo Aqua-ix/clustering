4e45c3da242a9a3a20cf8c153597a4f02acae583
Aqua-ix <souichi1997@gmail.com>
2019-02-01 08:41:39 +0900

    update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,17 @@
 
+;;; package.el
+(require 'package)
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; MELPA-stableを追加
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;; Marmaladeを追加
+(add-to-list 'package-archives  '("marmalade" . "https://marmalade-repo.org/packages/"))
+;; Orgを追加
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+;; 初期化
+(package-initialize)
+
 ;; 環境を日本語、UTF-8にする
 (set-locale-environment nil)
 (set-language-environment "Japanese")
