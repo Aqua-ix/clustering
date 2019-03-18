9b1f3d86a2acb99ac8a9823a0fa9c8a6905e7143
Aqua-ix <souichi1997@gmail.com>
2019-03-19 01:43:00 +0900

    update thesis

---
+++ thesis/src/qfcm.h
@@ -0,0 +1,23 @@
+#include<cmath>
+#include<cfloat>
+#include"matrix.h"
+#include"hcm.h"
+#include"sfcm.h"
+#include"efcm.h"
+
+#ifndef __QFCM__
+#define __QFCM__
+
+class Qfcm: public Sfcm, public Efcm{
+public:
+  Qfcm(int dimension,
+       int data_number,
+       int centers_number,
+       double fuzzifierEm,
+       double fuzzifierLambda);
+  void revise_membership(void);
+  void revise_centers(void);
+};
+
+#endif
+
