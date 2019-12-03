2e4204a3444ff4525b6003553c024ed400740f63
Aqua-ix <souichi1997@gmail.com>
2019-12-03 23:54:36 +0900

    2019-12-03

---
+++ result_actual.py
@@ -0,0 +1,88 @@
+#! /usr/bin/env python
+
+import glob
+import os
+import re
+import pathlib
+import pandas as pd
+
+data_dir="data/result_data/"
+b_e_q = ['B', 'E', 'Q']
+f_p = ['FCS', 'PCS']
+m_a = ['MAE', 'AUC']
+overlap = '_OVERLAP_'
+data_name = 'book'
+mp = 4
+clusters = [15, 35, 11]
+
+regrex = re.compile('[0-9]+[._]?[0-9]*')
+
+for m1_index, m1 in enumerate(b_e_q):
+    for m2 in f_p:
+        if m2 == 'PCS':
+            cluster = 1
+        else:
+            cluster = clusters[m1_index]
+
+        cn = '/clusters_number' + str(cluster)
+        
+        method_name = m1 + m2 + overlap
+        method_dir = data_dir + method_name + data_name
+        
+        for m3 in m_a:
+            miss = '/**/**/missing_pattern' + str(mp)
+            file_name = 'average'+ m3 +'.txt'
+            input_name = method_dir + cn + miss  + '/' + method_name + file_name
+            print('========== ' + m3 + ' in ' + method_dir + ' ==========')
+            files = sorted(glob.glob(input_name))
+            out = []
+            prev_tname = ''
+            tname_array = []
+            if 'result' in locals():
+                del result
+            
+            for file in files:
+                p_file = pathlib.Path(file)
+                threshold = p_file.parents[2]
+                param = "".join(regrex.findall(p_file.parents[1].name))
+            
+                #print(threshold.name + ', ' + param + ': ' + str(p_file))
+            
+                if threshold.name != prev_tname:
+                    tname_array.append(threshold.name)
+                    if 'result' in locals():
+                        out.append(result)
+                        del result
+                prev_tname = threshold.name
+            
+                table = (pd.read_table(str(p_file), names=('miss', param)))
+                if 'result' not in locals():
+                    result = table
+                else:
+                    result = pd.merge(result, table)
+            if not result.equals(table):
+                out.append(result)
+                
+            for index, o in enumerate(out):
+                misses = o['miss']
+                del o['miss']
+                print(tname_array[index])
+                out_dir = data_name + '_MP' + str(mp) + '_' + str(tname_array[index])
+                out_path = data_dir + m3 + '/'  + data_name + '/' + out_dir + '/'
+                if not os.path.exists(out_path):
+                    os.makedirs(out_path)
+                #print(out_path)
+                if(m3 == 'MAE'):
+                    index = o.idxmin(axis='columns')
+                    res = o.min(axis='columns')
+                    i_r = pd.concat([index, res], axis = 'columns')
+                    output = pd.concat([misses, i_r], axis = 'columns')
+                    output.to_csv(out_path + method_name + file_name, sep = '\t', header=False, index = False)
+                    #print(output)
+                else:
+                    res = o.max(axis='columns')
+                    index = o.idxmax(axis='columns')
+                    i_r = pd.concat([index, res], axis = 'columns')
+                    output = pd.concat([misses, i_r], axis = 'columns')
+                    output.to_csv(out_path + method_name + file_name, sep = '\t', header=False, index = False)
+                    #print(output)
---
+++ result_artificiality.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+
+import glob
+import os
+import re
+import pathlib
+import pandas as pd
+
+data_dir="data/result_data/"
+b_e_q = ['B', 'E', 'Q']
+f_p = ['FCS', 'PCS']
+m_a = ['MAE']
+overlap = '_OVERLAP_'
+data_name = 'artificiality'
+mp = 19
+
+regrex = re.compile('[0-9]+[.]?[0-9]*')
+
+for m1 in b_e_q:
+    for m2 in f_p:
+        method_name = m1 + m2 + overlap
+        method_dir = data_dir + method_name + data_name
+        for m3 in m_a:
+            miss = '/**/**/**/missing_pattern' + str(mp)
+            file_name = 'average'+ m3 +'.txt'
+            input_name = method_dir + miss  + '/' + method_name + file_name
+            print('========== ' + m3 + ' in ' + method_dir + ' ==========')
+            files = sorted(glob.glob(input_name))
+            out = []
+            prev_tname = ''
+            tname_array = []
+            if 'result' in locals():
+                del result
+            
+            for file in files:
+                p_file = pathlib.Path(file)
+                threshold = p_file.parents[2]
+                param = "_".join(regrex.findall(p_file.parents[1].name))
+            
+                #print(threshold.name + ', ' + param + ': ' + p_file.name)
+            
+                if threshold.name != prev_tname:
+                    tname_array.append(threshold.name)
+                    if 'result' in locals():
+                        out.append(result)
+                        del result
+                prev_tname = threshold.name
+            
+                table = (pd.read_table(str(p_file), names=('miss', param)))
+                if 'result' not in locals():
+                    result = table
+                else:
+                    result = pd.merge(result, table)
+            if not result.equals(table):
+                out.append(result)
+
+                
+            for index, o in enumerate(out):
+                misses = o['miss']
+                del o['miss']
+                print(tname_array[index])
+                out_dir = data_name + '_MP' + str(mp) + '_' + str(tname_array[index])
+                out_path = data_dir + m3 + '/'  + data_name + '/' + out_dir + '/'
+                if not os.path.exists(out_path):
+                    os.makedirs(out_path)
+                #print(out_path)
+                if(m3 == 'MAE'):
+                    index = o.idxmin(axis='columns')
+                    res = o.min(axis='columns')
+                    i_r = pd.concat([index, res], axis = 'columns')
+                    output = pd.concat([misses, i_r], axis = 'columns')
+                    output.to_csv(out_path + method_name + file_name, sep = '\t', header=False, index = False)
+                    #print(output)
+                else:
+                    res = o.max(axis='columns')
+                    index = o.idxmax(axis='columns')
+                    i_r = pd.concat([index, res], axis = 'columns')
+                    output = pd.concat([misses, i_r], axis = 'columns')
+                    output.to_csv(out_path + method_name + file_name, sep = '\t', header=False, index = False)
+                    #print(output)
