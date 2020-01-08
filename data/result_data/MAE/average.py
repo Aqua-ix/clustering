0c870caa6beab7797a0865082df226565f187b5e
Aqua-ix <souichi1997@gmail.com>
2020-01-08 18:10:00 +0900

    2019-01-08

--- data/result_data/MAE/average.py
+++ data/result_data/MAE/average.py
@@ -1,19 +1,20 @@
 #! /usr/bin/env python
 import csv
 import os
-
+import numpy as np
+ 
 mp = '4'
 ots = ['0.200000',
        '0.400000',
        '0.600000',
        '0.800000',
-       '1.000000',]
+       '1.000000']
 
 datasets = ['book', 'movie', 'libimseti', 'jester', 'epinions', 'sushi']
 methods = ['BFCS', 'EFCS', 'QFCS', 'BPCS', 'EPCS', 'QPCS']
-results = [0] * len(methods)
+results = np.empty(len(methods))
 
-for ot in ots:
+for index, ot in enumerate(ots):
     for data in datasets:
         inputDir = data + '/' + data + '_MP' + mp + '_overlap_threshold' + ot + '/'
         outputDir = data + '_MP' + mp + '_overlap/'
@@ -24,7 +25,8 @@ for ot in ots:
             for row in tsv:
                 missing = row[0]
                 param = row[1]
-                
+                mae = row[2]
+            results[0].append(mae)
                 
             inputFile.close()
 
